82a96ad057cfb1300e21ef86fd4fa6dc024611bd
germix <german.m7186@gmail.com>
2017-09-10 20:01:34 -0300

    Login

---
+++ api/src/cmds/Command_Auth_Login.php
@@ -0,0 +1,34 @@
+<?php
+
+class Command_Auth_Login implements Command
+{
+	public function run($method, $uriParameters, $queryParameters)
+	{
+		$ret = HTTP_RCODE___OK;
+		$json = array();
+		$username = getQueryParameter($queryParameters, 'username');
+		$password = getQueryParameter($queryParameters, 'password');
+		if(empty($username) || empty($password))
+		{
+			$json['ret'] = ERROR___BAD_PARAMETERS;
+			// ...
+			echo json_encode($json);
+			return HTTP_RCODE___BAD_REQUEST;
+		}
+		// ...
+		$token = Authentication::login($username, $password);
+		if(is_integer($token))
+		{
+			$json['ret'] = $token;
+		}
+		else
+		{
+			$json['ret'] = SUCCESS;
+			$json['token'] = $token;
+		}
+		echo json_encode($json);
+		return HTTP_RCODE___OK;
+	}
+}
+
+?>
