6c3e0d027eb6e0ac3ed065c88767195468b57da0
germix <german.m7186@gmail.com>
2017-09-13 03:00:46 -0300

    Se eliminó instancia innecesaria de Connection

--- api/src/cmds/Command_Products.php
+++ api/src/cmds/Command_Products.php
@@ -6,7 +6,6 @@ class Command_Products implements Command
 {
 	public function run($method, $uriParameters, $queryParameters)
 	{
-		$con = Connection::getInstance();
 		if($method == 'GET')
 		{
 			$mode = getQueryParameter($queryParameters, 'mode');
