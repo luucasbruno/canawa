01edabdc14b0294b37190108ac0e4a5764d2c5ca
germix <german.m7186@gmail.com>
2017-09-30 16:42:25 -0300

    Ya no se devuelve el error http 400, sino que se trata con valores de errores los campos faltantes

--- api/src/cmds/Command_Auth_Login.php
+++ api/src/cmds/Command_Auth_Login.php
@@ -7,6 +7,7 @@ class Command_Auth_Login implements Command
 		$json = array();
 		$username = getQueryParameter($queryParameters, 'username');
 		$password = getQueryParameter($queryParameters, 'password');
+		/*
 		if(empty($username) || empty($password))
 		{
 			$json['ret'] = ERROR___BAD_PARAMETERS;
@@ -14,6 +15,7 @@ class Command_Auth_Login implements Command
 			echo json_encode($json);
 			return HTTP_RCODE___BAD_REQUEST;
 		}
+		*/
 		// ...
 		$token = Authentication::login($username, $password);
 		if(is_integer($token))
